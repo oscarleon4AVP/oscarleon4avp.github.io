cdbaa22ea0560dacf08f0979b8f4b760b39e9355
oscarleon4AVP <124410665+oscarleon4AVP@users.noreply.github.com>
2023-04-26 20:05:56 -0600

    26/04/23 relaciones de base de datos

--- usuarios.sql
+++ usuarios.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-02-2023 a las 01:43:54
+-- Tiempo de generación: 27-04-2023 a las 04:05:25
 -- Versión del servidor: 10.4.25-MariaDB
 -- Versión de PHP: 8.1.10
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `tienda_online`
+-- Base de datos: `spotify`
 --
 
 -- --------------------------------------------------------
@@ -29,28 +29,23 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(100) NOT NULL,
-  `correo` varchar(100) NOT NULL,
-  `telefono` varchar(20) NOT NULL,
-  `fecah_nacimiento` date NOT NULL,
-  `sexo` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `nombre` varchar(100) COLLATE utf8_bin NOT NULL,
+  `correo` varchar(100) COLLATE utf8_bin NOT NULL,
+  `contrasena` varchar(100) COLLATE utf8_bin NOT NULL,
+  `usuario` varchar(30) COLLATE utf8_bin NOT NULL,
+  `telefono` varchar(11) COLLATE utf8_bin NOT NULL,
+  `membresias_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nombre`, `correo`, `telefono`, `fecah_nacimiento`, `sexo`) VALUES
-(1, 'pepe', 'pepe@gmail.com', '667999999', '2023-02-01', 1),
-(2, 'julio', 'juio@hotmail.com', '6671234567', '2023-02-15', 1),
-(3, 'maria', 'mari@gmail.com', '6671098765', '2023-01-02', 2),
-(4, 'juana', 'juana@gmail.com', '6674526789', '2019-02-06', 2),
-(5, 'mondongo', 'mondongo23@gmail.com', '6671234585', '2023-02-13', 1),
-(6, 'arturo', 'arturo@gmail.com', '6674526737', '2022-02-08', 1),
-(7, 'marta', 'marta@gmail.com', '6674541789', '2021-10-14', 2),
-(8, 'carla', 'carla@gmail.com', '6671874585', '2013-05-09', 2),
-(9, 'justino', 'justi@hotmail.com', '6674586237', '2002-09-12', 1),
-(10, 'luisa', 'luisa@gmail.com', '6671629565', '2007-02-06', 2);
+INSERT INTO `usuarios` (`id`, `nombre`, `correo`, `contrasena`, `usuario`, `telefono`, `membresias_id`) VALUES
+(1, 'David', 'davidreginoramirezhernandez4@gmail.com', '20062009', 'DavidRegino', '6673023087', 2),
+(2, 'Kyan', 'kyan@gmail.com', 'minecraft', 'KC8', '6694872', 1),
+(3, 'Alan', 'castro@gmail.com', 'destiny', 'Flainxps', '6631475147', 2),
+(4, 'pepe', 'pepe@gmail.com', 'pepe123', 'papucho', '6671898532', 1);
 
 --
 -- Índices para tablas volcadas
@@ -60,7 +55,8 @@ INSERT INTO `usuarios` (`id`, `nombre`, `correo`, `telefono`, `fecah_nacimiento`
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `membresias_id` (`membresias_id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -70,7 +66,17 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`membresias_id`) REFERENCES `membresias` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
