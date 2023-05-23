9fe2fb95021b492a42b2769210a9f244e5e27410
oscarleon4AVP <124410665+oscarleon4AVP@users.noreply.github.com>
2023-05-22 18:00:13 -0700

    22/05/23 php hola mundo

---
+++ parcial3/practica1.php
@@ -0,0 +1,20 @@
+<?php
+
+    echo "<h1>hola mundo</h1>";
+
+    echo"<hr>";
+
+    $nombre = "Luis";
+    $edad = 18;
+
+    if($edad >= 18) {
+        echo $nombre . " es mayor de edad";
+    } else{
+        echo $nombre . " es menor de edad";
+    }
+    echo "<br><br>";
+
+    for($i=0; $i<10; $i++){
+        echo $i . "<br>";
+    }
+?>
