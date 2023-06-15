4a35b6f4e3b822c9d63bb8cd618a661e5dad5947
oscarleon4AVP <124410665+oscarleon4AVP@users.noreply.github.com>
2023-06-14 19:22:54 -0700

    14/06/23 serpientes y escaleras

--- parcial3/practica7.php
+++ parcial3/practica7.php
@@ -20,6 +20,16 @@
         .verde { background-color: rgb(165,204,59); }
         .azul { background-color: rgb(1,173,239); }
         .morado { background-color: rgb(131,78,160); }
+        .escalera {
+            background-image: url('escalera.png');
+            background-size: cover;
+            background-position:center;
+        }
+        .serpiente {
+            background-image: url('serpiente.png');
+            background-size: cover;
+        }
+
     </style>
 </head>
 <body>
@@ -28,13 +38,51 @@
         <div class="row">
             <?php
             $colors = ['rojo', 'amarillo', 'verde', 'azul', 'morado'];
+            $escaleras = [];
+            $serpientes = [];
+
+            // Generar escaleras
+            for ($i = 0; $i < 5; $i++) {
+                $inicio_e = rand(2, 90); // Casilla de inicio de la escalera
+                $fin_e = rand($inicio_e + 10, 100); // Casilla de fin de la escalera
+                $escaleras[] = [$inicio_e, $fin_e];
+            }
+
+            // Generar serpientes
+            for ($i = 0; $i < 4; $i++) {
+                $inicio_s = rand(11, 100); // Casilla de inicio de la serpiente
+                $fin_s = rand(1, $inicio_s - 1); // Casilla de fin de la serpiente
+                $serpientes[] = [$inicio_s, $fin_s];
+            }
 
             for ($i = 100; $i > 0; $i--) {
                 $randomColor = $colors[array_rand($colors)];
-                echo "<div class='col-1'><div class='casilla $randomColor'>$i</div></div>";
+                $isEscalera = false;
+                $isSerpiente = false;
+
+                // Comprobar si la casilla es una de las casillas de inicio o fin de alguna escalera
+                foreach ($escaleras as $escalera) {
+                    if ($i == $escalera[0] || $i == $escalera[1]) {
+                        $isEscalera = true;
+                        break;
+                    }
+                }
+
+                // Comprobar si la casilla es una de las casillas de inicio o fin de alguna serpiente
+                foreach ($serpientes as $serpiente) {
+                    if ($i == $serpiente[0] || $i == $serpiente[1]) {
+                        $isSerpiente = true;
+                        break;
+                    }
+                }
+
+                $escaleraClass = $isEscalera ? 'escalera' : '';
+                $serpienteClass = $isSerpiente ? 'serpiente' : '';
+                echo "<div class='col-1'><div class='casilla $randomColor $escaleraClass $serpienteClass'>$i</div></div>";
             }
             ?>
         </div>
     </div>
 </body>
 </html>
+
