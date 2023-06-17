41783dae5359916e4c70dfce5b6e0e014a4431ac
oscarleon4AVP <124410665+oscarleon4AVP@users.noreply.github.com>
2023-06-16 18:58:57 -0700

    16/06/23 serpientes y escaleras

--- parcial3/practica7.php
+++ parcial3/practica7.php
@@ -8,6 +8,18 @@
     <link rel="stylesheet" href="css/bootstrap.css">
     <style>
         .casilla {
+            border: 1px solid #000;
+            text-align: center;
+            padding: 17px 0;
+            font-size: 20px;
+            font-weight: bold;
+            margin-bottom: -1px;
+        }
+        .rojo { background-color: red; }
+        .amarillo { background-color: rgb(254,242,0); }
+        .verde { background-color: rgb(165,204,59); }
+        .azul { background-color: rgb(1,173,239); }
+        .casilla {
             flex-grow: 1;
             border: 1px solid #000;
             text-align: center;
@@ -15,21 +27,13 @@
             font-size: 20px;
             font-weight: bold;
         }
-        .rojo { background-color: red; }
-        .amarillo { background-color: rgb(254,242,0); }
-        .verde { background-color: rgb(165,204,59); }
-        .azul { background-color: rgb(1,173,239); }
-        .morado { background-color: rgb(131,78,160); }
-        .escalera {
-            background-image: url('escalera.png');
-            background-size: cover;
-            background-position:center;
-        }
         .serpiente {
             background-image: url('serpiente.png');
             background-size: cover;
         }
+        .ficha{
 
+        }
     </style>
 </head>
 <body>
@@ -37,56 +41,38 @@
         <h1>Serpientes y Escaleras</h1>
         <form action="practica7.php" method="post">
         <input type="submit" value="jugador" class="btn btn-succes"><hr>
-        <div class="row">
-            <?php
-            $colors = ['rojo', 'amarillo', 'verde', 'azul', 'morado'];
-            $escaleras = [];
-            $serpientes = [];
-
-            // Generar escaleras
-            for ($i = 0; $i < 5; $i++) {
-                $inicio_e = rand(2, 90); // Casilla de inicio de la escalera
-                $fin_e = rand($inicio_e + 10, 100); // Casilla de fin de la escalera
-                $escaleras[] = [$inicio_e, $fin_e];
-            }
-
-            // Generar serpientes
-            for ($i = 0; $i < 4; $i++) {
-                $inicio_s = rand(11, 100); // Casilla de inicio de la serpiente
-                $fin_s = rand(1, $inicio_s - 1); // Casilla de fin de la serpiente
-                $serpientes[] = [$inicio_s, $fin_s];
-            }
-
-            for ($i = 100; $i > 0; $i--) {
-                $randomColor = $colors[array_rand($colors)];
-                $isEscalera = false;
-                $isSerpiente = false;
-
-                // Comprobar si la casilla es una de las casillas de inicio o fin de alguna escalera
-                foreach ($escaleras as $escalera) {
-                    if ($i == $escalera[0] || $i == $escalera[1]) {
-                        $isEscalera = true;
-                        break;
+            <div class="row" style="margin-bottom: -1px;">
+                
+                <?php
+                $colores = ['rojo', 'amarillo', 'verde', 'azul'];
+                if(isset($_POST["csilla"])){
+                    $dado = rand(1,12);
+                    echo "<h2> Resultado del Dado = ".$dado."</h2>";
+                    $casilla = $dado + intval($_POST["casilla"]);
+                    echo "<h3>El jugador estaba en la casilla".intval($_POST["casilla"])." y paso a la casilla".$casilla;
+                    //valida si gano el jugador
+                    if($casilla >= 100){
+                        echo "<h1> El jugador gano </h1>";
+                        $casilla = 100 ;
                     }
+                    //valida la escaler en la casilla 5
+                } else{
+                    $casilla = 0;}
+                for ($i = 100; $i > 0; $i--) {
+                    $randomColor = $colores[array_rand($colores)];
+                    echo "<div class='col-1'><div class='casilla $randomColor'>$i</div></div>";
+                }if($i == 5 || $i == 8 || $i == 36 || $i == 29 || $i == 42 || $i == 75 ||$i == 79 ||$i == 83){
+                    echo "<div class='col-1 card m-1 casilla escalera.png' style='background-color:".$colores[rand(0,6)].";'>".$i."</div>";
+                } else if($i == 17 || $i == 54 || $i == 58 || $i == 64 || $i == 87 || $i == 93 ||$i == 95 ||$i == 99 ||$i == 36){
+                    echo "<div class='col-1 card m-1 casilla serpiente' style='background-color:".$colores[rand(0,6)].";'>".$i."</div>";
+                } else {
+                    echo "<div class='col-1 card m-1 casilla' style='background-color:".$colores[rand(0,6)].";'>".$i."</div>";
                 }
-
-                // Comprobar si la casilla es una de las casillas de inicio o fin de alguna serpiente
-                foreach ($serpientes as $serpiente) {
-                    if ($i == $serpiente[0] || $i == $serpiente[1]) {
-                        $isSerpiente = true;
-                        break;
-                    }
-                }
-
-                $escaleraClass = $isEscalera ? 'escalera' : '';
-                $serpienteClass = $isSerpiente ? 'serpiente' : '';
-                echo "<div class='col-1'><div class='casilla $randomColor $escaleraClass $serpienteClass'>$i</div></div>";
-            }
-            ?>
-        </div>
+                ?>
+            </div>
+            <input type="hidden" name="casilla" value="<?php echo $casilla;?>">
+        </form>    
     </div>
-        </form>
-        
 </body>
 </html>
 
